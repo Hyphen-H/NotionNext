6601cd7ef273ec16c36a1866df4c13f8190c5ddf
CBE <103420806+Hyphen-H@users.noreply.github.com>
2024-10-20 19:31:23 +0400

    LXGW https://fonts.zeoseven.com/zh-CN/font/cn/s/292/

--- blog.config.js
+++ blog.config.js
@@ -63,14 +63,14 @@ const BLOG = {
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
-    //'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
-    //'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+    'https://static.zeoseven.com/cn/292/main/result.css',
+    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     '"Noto Sans"',
     //'"LXGW WenKai"',
     //'"PingFang SC"',
@@ -92,7 +92,7 @@ const BLOG = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     '"Noto Serif"',
   ],
   FONT_AWESOME:
